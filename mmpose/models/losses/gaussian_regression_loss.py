ef07387a244c25a3b1ae63fc0a662196e258fc08
aevns <evans.andrew@live.com>
2025-11-12 21:52:20 -0800

    Creation of relevant files, initial content

---
+++ mmpose/models/losses/gaussian_regression_loss.py
@@ -0,0 +1,48 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import math
+from functools import partial
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+from mmpose.registry import MODELS
+
+
+@MODELS.register_module()
+class GaussianLoss(nn.Module):
+
+    def __init__(self,
+                 use_target_weight=False,
+                 use_label_loss=True,
+                 loss_weight=1.0):
+        super.__init__()
+        self.use_target_weight = use_target_weight
+        self.use_label_loss = use_label_loss
+        self.loss_weight = loss_weight
+        self.cov_idx = torch.tensor([[2,4],[4,3]])
+    
+    def forward(self, output, target, target_weight = None):
+
+        pose = output[:,:,0:2]
+        cov_mat = output[:,:,self.cov_idx]
+        labeled = output[:,:,5]
+        
+        gt_pose = target[:,:,0:2]
+        mask = (target[:,:,2] != 0)
+        dif = torch.reshape(gt_pose - pose, (pose.shape[0], pose.shape[1], pose.shape[2], 1))
+        q = torch.matmul(torch.transpose(dif,-1,-2), torch.matmul(torch.inverse(cov_mat), dif))
+        q = q.view(q.shape[0], q.shape[1])
+        if self.use_target_weight:
+            assert target_weight is not None
+            pose_loss = torch.sum(target_weight * (mask * (torch.log(torch.det(cov_mat)) + q)/2 + 1.8378770664093455), dim=(-1))
+        else:
+            pose_loss = torch.sum(mask * (torch.log(torch.det(cov_mat)) + q)/2 + 1.8378770664093455, dim=(-1))
+        # Logs are taken from (0.00001, 1] for numerical stability
+        # This ensures wildly wrong predictions don't have exploding gradients
+        if self.use_label_loss:
+            label_loss = -torch.log(1 - labeled * (1 - 1E-5))
+            label_loss[mask] = -torch.log(1E-5 + labeled[mask] * (1 - 1E-5))
+            pose_loss += torch.sum(label_loss, dim=-1)
+    
+        return pose_loss * self.loss_weight
